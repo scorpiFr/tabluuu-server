99bc006ddd30954eebc951d1ed6520f4e1d5f011
Scorpi <scorpifr@gmail.com>
2025-06-09 15:26:36 +0200

    Ajout de la route getQrCode .

--- routes/getfile.js
+++ routes/getfile.js
@@ -5,9 +5,12 @@ const multer = require("multer");
 const upload = multer(); // pas de stockage, en mémoire
 const path = require("path");
 const fs = require("fs");
+const QRCode = require("qrcode");
+const { createCanvas, loadImage } = require("canvas");
 
 // Dossier racine autorisé
 const BASE_DIR = path.resolve(process.env.UPLOAD_FILE_PATH);
+const PUBLIC_DIR = path.resolve("./public/");
 
 // GET /test/getFile
 router.get("/getFile", (req, res) => {
@@ -46,6 +49,84 @@ router.get("/getFile", (req, res) => {
         res.status(500).send("Error sending file.");
       }
     });
+  });
+});
+
+//-----------------------------------------------------------
+function generateQrMenu(barid, table) {
+  const proforma_menu = path.resolve(PUBLIC_DIR, "proforma-menu-kebab.png");
+  return generateQrImage(proforma_menu, barid, table);
+}
+
+function generateQrCarte(barid, table) {
+  const proforma_menu = path.resolve(PUBLIC_DIR, "proforma-carte-bar.png");
+  return generateQrImage(proforma_menu, barid, table);
+}
+
+async function generateQrImage(proformaPath, barid, table) {
+  const qrText = `http://tabluuu.fr:3000/?barid=${barid}&table=${table}`;
+  const qrSize = 500;
+
+  // Charger le proforma
+  const proforma = await loadImage(proformaPath);
+
+  // Générer le QR code en dataURL
+  const qrDataUrl = await QRCode.toDataURL(qrText, {
+    width: qrSize,
+    margin: 1,
+  });
+  const qrImage = await loadImage(qrDataUrl);
+
+  // dessiner l'image
+  const canvas = createCanvas(614, 874);
+  const ctx = canvas.getContext("2d");
+  ctx.drawImage(proforma, 0, 0);
+  ctx.drawImage(qrImage, 60, 200);
+
+  return canvas;
+}
+
+router.get("/getqrcode", async (req, res) => {
+  const barid = req.query.barid;
+  const table = req.query.table;
+
+  if (!barid || !table) {
+    return res.status(400).send("Missing barid or table parameter");
+  }
+
+  // Chemin du dossier et du fichier
+  const dirPath = path.join(BASE_DIR, barid);
+  const fileName = `qrcode_${table}.jpg`;
+  const filePath = path.join(dirPath, fileName);
+
+  // Vérifier si le fichier existe
+  fs.access(filePath, fs.constants.F_OK, async (err) => {
+    if (!err) {
+      // Fichier existe, on le renvoie directement
+      return res.sendFile(filePath);
+    }
+
+    try {
+      // Créer le dossier s'il n'existe pas
+      if (!fs.existsSync(dirPath)) {
+        fs.mkdirSync(dirPath, { recursive: true });
+      }
+
+      // URL à encoder dans le QR code
+      const urlToEncode = `http://tabluuu.fr:3000/?barid=${barid}&table=${table}`;
+      //const canvas = await generateQrMenu(barid, table);
+      const canvas = await generateQrCarte(barid, table);
+      const buffer = canvas.toBuffer("image/png");
+
+      // Sauvegarde
+      fs.writeFileSync(filePath, buffer);
+
+      // Sauvegarde
+      return res.sendFile(filePath);
+    } catch (error) {
+      console.error("Erreur dans la génération du QR code:", error);
+      return res.status(500).send("Erreur serveur");
+    }
   });
 });
 
