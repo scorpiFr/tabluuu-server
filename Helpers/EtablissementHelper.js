482eb47cce18f381e9b901ab94e3483417105179
Scorpi <scorpifr@gmail.com>
2025-07-08 06:04:15 +0200

    Add testemail + create etablissement.

--- Helpers/EtablissementHelper.js
+++ Helpers/EtablissementHelper.js
@@ -1,5 +1,6 @@
 const crypto = require("crypto");
 require("dotenv").config();
+const { sequelize } = require("../models");
 
 function encryptPassword(password) {
   const salt = process.env.SALT;
@@ -23,4 +24,94 @@ function removeSecretInformations(etablissement) {
   return etablissement;
 }
 
-module.exports = { encryptPassword, createSecretKey, removeSecretInformations };
+function isStrictDecimal(str) {
+  return /^(-?\d+(\.\d+)?|-?\.\d+)$/.test(str);
+}
+
+async function getNbrEmailsUser(email) {
+  // test
+  try {
+    const [results, metadata] = await sequelize.query(
+      "SELECT COUNT(id) AS nbr FROM user WHERE email = :email",
+      {
+        replacements: { email: email },
+        type: sequelize.QueryTypes.SELECT,
+      }
+    );
+
+    // results sera un tableau d'objets, ex : [{ position: 5 }]
+    return [results.nbr, null];
+  } catch (error) {
+    console.error("Erreur dans getNbrEmailsUser:", error);
+    return [0, error];
+  }
+}
+
+async function getNbrEmailsEtablissement(email) {
+  // test
+  try {
+    const [results, metadata] = await sequelize.query(
+      "SELECT COUNT(id) AS nbr FROM etablissement WHERE email_facturation = :email OR email_commandes = :email",
+      {
+        replacements: { email: email },
+        type: sequelize.QueryTypes.SELECT,
+      }
+    );
+
+    // results sera un tableau d'objets, ex : [{ position: 5 }]
+    return [results.nbr, null];
+  } catch (error) {
+    console.error("Erreur dans getNbrEmailsEtablissement:", error);
+    return [0, error];
+  }
+}
+
+// test if email exists on the system.
+// return true if email exists.
+async function testEmail(email) {
+  const [nbrEmailEtablissement, tmp1] = await getNbrEmailsEtablissement(email);
+  if (nbrEmailEtablissement > 0) {
+    return true;
+  }
+
+  const [nbrEmailUser, tmp2] = await getNbrEmailsUser(email);
+  if (nbrEmailUser > 0) {
+    return true;
+  }
+
+  return false;
+}
+
+// verify if email is syntaxically valid
+function emailIsValid(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
+
+function getRandomPassword(width = 7) {
+  const caracts = "abcdefghijklmnopqrstuvwxyz0123456789";
+  let res = "";
+  let len = caracts.length;
+
+  for (let i = 0; i < width; i++) {
+    const index = Math.floor(Math.random() * len);
+    res += caracts[index];
+  }
+
+  return res;
+}
+
+function getRandomPasswordEncrypted() {
+  return encryptPassword(getRandomPassword());
+}
+
+module.exports = {
+  encryptPassword,
+  createSecretKey,
+  removeSecretInformations,
+  isStrictDecimal,
+  testEmail,
+  emailIsValid,
+  getRandomPassword,
+  getRandomPasswordEncrypted,
+};
--- routes/Etablissement.js
+++ routes/Etablissement.js
@@ -1,15 +1,151 @@
 const express = require("express");
 const router = express.Router();
-const { Etablissement } = require("../models");
+const { sequelize, Etablissement } = require("../models");
 const {
   removeSecretInformations,
   encryptPassword,
+  isStrictDecimal,
+  testEmail,
+  emailIsValid,
+  createSecretKey,
+  getRandomPassword,
+  getRandomPasswordEncrypted,
 } = require("../Helpers/EtablissementHelper.js");
 
 const multer = require("multer");
 const upload = multer(); // pas de stockage, en mémoire
 const auth = require("../middleware/auth.js");
 const { emtyEtablissementCache } = require("../modules/APIEtablissementCache");
+
+// test email
+router.get("/testemail", auth, async (req, res, next) => {
+  // verif params
+  const email = req.query.email;
+  if (!email) {
+    return res.status(400).json({ error: "Mandatory fields : email" });
+  }
+  // verify session
+  if (!req.Session) {
+    return res.status(401).json({ erreur: "Bad auth token" });
+  }
+  if (req.Session.role === "admin");
+  else if (req.Session.role === "commercial");
+  else if (req.Session.role === "etablissement");
+  else {
+    return res.status(403).json({ erreur: "Forbidden" });
+  }
+
+  // actions
+  const emailExists = await testEmail(email);
+
+  // return
+  if (emailExists) {
+    res.json({ status: 0, msg: "Email non-disponible" });
+  } else {
+    res.json({ status: 1, msg: "Email disponible" });
+  }
+});
+
+// CREATE
+router.post("/", auth, async (req, res, next) => {
+  // verify inputs
+  let {
+    email_facturation,
+    email_commandes,
+    nom_etablissement,
+    type,
+    nom,
+    prenom,
+    adresse,
+    tel,
+    type_contrat,
+    prix,
+    is_allocated,
+  } = req.body;
+  let inputErrors = [];
+  if (!email_facturation || !emailIsValid(email_facturation)) {
+    inputErrors.push("email_facturation");
+  }
+  if (email_commandes.length > 0 && !emailIsValid(email_commandes)) {
+    inputErrors.push("email_commandes");
+  }
+  if (!nom_etablissement) {
+    inputErrors.push("nom_etablissement");
+  }
+  if (!type || (type != "bar" && type != "kebab" && type != "restaurant")) {
+    inputErrors.push("type");
+  }
+  if (
+    !type_contrat ||
+    (type_contrat != "image" && type_contrat != "menu" && type != "commande")
+  ) {
+    inputErrors.push("type_contrat");
+  }
+  if (!prix || !isStrictDecimal(prix)) {
+    inputErrors.push("prix");
+  }
+  if (is_allocated != 0 && is_allocated != 1) {
+    is_allocated = "0";
+  }
+  if (inputErrors.length > 0) {
+    return res
+      .status(400)
+      .json({ error: "Mandatory fields : " + inputErrors.join() });
+  }
+
+  // verify if email exists
+  const emailExists = await testEmail(email_facturation);
+  if (emailExists) {
+    res.status(403).json({
+      msg: "Email already exists on the system - " + email_facturation,
+    });
+  }
+  if (email_commandes.length > 0) {
+    const emailExists2 = await testEmail(email_commandes);
+    if (emailExists2) {
+      res.status(403).json({
+        msg: "Email already exists on the system - " + email_commandes,
+      });
+    }
+  }
+
+  // verify session
+  if (!req.Session) {
+    return res.status(401).json({ erreur: "Bad auth token" });
+  }
+  if (req.Session.role === "admin");
+  else if (req.Session.role === "commercial");
+  else {
+    return res.status(403).json({ erreur: "Forbidden" });
+  }
+
+  // create
+  try {
+    const password = getRandomPasswordEncrypted();
+    const secret_key = createSecretKey();
+    let created = await Etablissement.create({
+      email_facturation,
+      email_commandes,
+      nom_etablissement,
+      type,
+      nom,
+      prenom,
+      adresse,
+      tel,
+      type_contrat,
+      prix,
+      is_allocated,
+      password,
+      secret_key,
+    });
+
+    // return
+    created = removeSecretInformations(created);
+    res.status(201).json(created);
+  } catch (err) {
+    next(err);
+  }
+});
 
 // GET all
 router.get("/", auth, async (req, res, next) => {
